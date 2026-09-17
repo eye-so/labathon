7c5eb932bd44636c50032459aa3999845b763e7e
eye-so <kousikraguludayakumar@hotmail.com>
2026-09-17 23:05:27 +0530

    problem02 hw
    four functions to add two numbers

--- problem02.c
+++ problem02.c
@@ -1,2 +1,33 @@
 //2. Write a C program to add two numbers.
+#include <stdio.h>
 
+void input(int *a, int *b)
+{
+    printf("Enter two numbers ");
+    scanf("%d%d", a, b);
+}
+
+int add(int a, int b)
+{
+    return a+b;
+}
+
+void output(int a, int b, int sum)
+{
+    printf("%d + %d = %d\n", a, b, sum);
+}
+
+int process()
+{
+    int a, b, sum;
+    input(&a, &b);
+    sum = add(a, b);
+    output(a, b, sum);
+   
+    return 0;
+}
+
+int main()
+{
+   printf("%d\n", process());
+}
